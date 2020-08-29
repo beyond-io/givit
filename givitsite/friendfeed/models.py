8d4808d8834e4ab7b76ad8ac055a9fd0c97d47cc
liorsicd <lior.cd@gmail.com>
2020-08-29 19:17:03 +0300

    new app - freindfeed
    create logic for new feature - freind freindfeed
    - model to connect the 'founded' db
    - url to the feed page
    - view method called 'requestItem'

--- givitsite/friendfeed/models.py
+++ givitsite/friendfeed/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< HEAD
+class FoundItem(models.Model):
+    request_id = models.IntegerField()
+    url = models.CharField(max_length=200)
+    picture = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=12) 
+
+    
+=======
+>>>>>>> parent of ce13deb... update changes + add freindfeed
--- givitsite/friendfeed/views.py
+++ givitsite/friendfeed/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
+
+def requestItem(request):
+
+    if request.method == 'GET':
+        founditems = FoundItem.objects.all()
+        return render (request, 'feed.html', {'founds':founditems})
+
+=======
+>>>>>>> parent of ce13deb... update changes + add freindfeed
